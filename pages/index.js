8607b2885f3d95c03440f485d7717dcf3a745659
Orbt <gabrielb@adekz.com>
2021-10-06 22:36:16 -0300

    Error update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,9 @@
 import React, { Component } from "react";
 import Router from "next/router";
-import Context from "../contexts/HeaderContext";
+
 export default function Index() {
-  <Context.Provider>
   React.useEffect(() => {
     Router.push("/admin/dashboard")
   });
-  </Context.Provider>
-
   return <div />;
 }
